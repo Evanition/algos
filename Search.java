465f0f173069986a22483512770323b79210329a
Evanition <dewaynebarnes123@gmail.com>
2023-12-28 17:42:20 -0500

    outlining

--- Search.java
+++ Search.java
@@ -1,3 +1,25 @@
 public class Search {
+    int [][] arr;
+    int target;
+
+    // Linear search
+
+    // Sentinel Linear Search
+
+    // Binary Search
+
+    // Meta Binary Search
+
+    // Ternary Binary Search
+
+    // Jump Search
+
+    // Interpolation Search
+
+    // Exponential Search
+
+    // Fibonacci Search
+
+    // The Ubiquitious Binary Search
 
 }
---
+++ Sort.java
@@ -0,0 +1,51 @@
+public class Sort {
+    // In-place merge sort
+
+    // Heapsort
+
+    // Introsort
+
+    // Merge sort
+
+    // Tournament Sort
+
+    // Tree Sort
+
+    // Block Sort
+
+    // SmoothSort
+
+    // Timsort
+
+    // PatienceSort
+
+    // Cubesort
+
+    // QuickSort
+
+    // LibrarySort
+
+    // ShellSort
+
+    // CombSort
+
+    // Insertion sort
+
+    // Bubblesort
+
+    // Cocktail shaker sort
+
+    // GnomeSort
+
+    // odd-even sort
+
+    // simple panacake sort
+
+    // strand sort
+
+    // selection sort
+
+    // exchange sort
+
+    // cycle sort
+}
