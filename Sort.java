c57be445bdfe10bf120d49f4a7afe92b9043ed83
hrizwan3 <hrizwan5472013@gmail.com>
2023-12-28 19:49:56 -0500

    :3

--- Sort.java
+++ Sort.java
@@ -1,5 +1,3 @@
-import javafx.scene.layout.Priority;
-
 import java.util.*;
 
 public class Sort {
@@ -71,6 +69,30 @@ public class Sort {
     // bucket sort (non-uniform keys)
 
     // counting sort
+    static int max(int[] input) {
+        int out = input[0];
+        for (int x : input) {
+            out = Math.max(x, out);
+        }
+        return out;
+    }
+    public static int[] countingSort(int[] input) {
+        int max = max(input);
+        int[] intermediate = new int[max + 1];
+        int[] out = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            intermediate[input[i]]++;
+        }
+        for (int i = 1; i < intermediate.length; i++) {
+            intermediate[i] += intermediate[i - 1];
+        }
+        for (int i = input.length - 1; i >= 0; i--) {
+            out[intermediate[input[i]] - 1] = input[i];
+            intermediate[input[i]]--;
+        }
+        return out;
+    }
 
     // LSD Radix Sort
 
@@ -121,8 +143,8 @@ public class Sort {
     // conway sort
 
     public static void main(String[] args) {
-        int[] a = {5, 43, 6, 2, 7, 9, 2, -1, 100, -2000};
-        int[] test = heapSort(a);
+        int[] a = {5, 4, 6, 2, 7, 9, 2, 1, 10, 20};
+        int[] test = countingSort(a);
 
         System.out.println(Arrays.toString(test));
     }
